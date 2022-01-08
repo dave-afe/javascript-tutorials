d13d8af6dfcca78366ac1f1073e221e49bad9c28
Dave Afeliga <dave.ayomah@gmail.com>
2022-01-08 13:59:19 +0000

    Working with functions in javascript

---
+++ Lesson-1/hello-functions.js
@@ -0,0 +1,13 @@
+// Working with functions 
+function addNumbers() {
+    let sum = 3 + 4;
+    console.log(sum)
+}
+
+addNumbers()
+
+function squareOfNumber(number) {
+    return number ** 2
+}
+
+console.log(squareOfNumber(7));
