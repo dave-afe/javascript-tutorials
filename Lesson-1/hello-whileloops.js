d7c24ba33b1a24212229da515c3a7ea1760f73e0
Dave Afeliga <dave.ayomah@gmail.com>
2022-01-08 23:21:23 +0000

    Lesson on While Loops in javascipt

---
+++ Lesson-1/hello-whileloops.js
@@ -0,0 +1,25 @@
+const names = [
+    "Alex",
+    "Kweku",
+    "Ansa",
+    "Frost",
+    "Joe"
+];
+const levels = [
+    "creche",
+    "kindergaten",
+    "primary",
+    "junior high",
+    "senior high",
+    "polytechnic or university"
+];
+let number = 0;
+while(number < 5) {
+    console.log(number);
+    number++;
+};
+let i = 0;
+do{
+    console.log(levels[i]);
+    i++;
+}while(i<levels.length);
