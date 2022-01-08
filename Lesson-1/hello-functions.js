92bd0fe6d9bb0b7bd38b7ec68a3c3fb30c890c56
Dave Afeliga <dave.ayomah@gmail.com>
2022-01-08 22:32:38 +0000

    Made a few refactoring on the code

--- Lesson-1/hello-functions.js
+++ Lesson-1/hello-functions.js
@@ -3,11 +3,8 @@ function addNumbers() {
     let sum = 3 + 4;
     console.log(sum)
 }
-
-addNumbers()
-
+addNumbers();
 function squareOfNumber(number) {
     return number ** 2
-}
-
+};
 console.log(squareOfNumber(7));
