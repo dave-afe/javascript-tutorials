588dcc2b7240e4e0a7471c0b1b77025fa4d5511e
Dave Afeliga <dave.ayomah@gmail.com>
2022-01-08 13:15:07 +0000

    Lessons on variable creation and objects in javascript

--- Lesson-1/hello-world.js
+++ Lesson-1/hello-world.js
@@ -1,2 +1,17 @@
 let date = new Date(1999, 4, 28);
 console.log(date)
+
+
+// Lessons on Javascript Object's
+
+let person = {
+    firstName: "Jamila",
+    age: 21,
+    isFemale: true,
+    balance: 100.34,
+    birthDate: new Date(2000, 1, 28).toJSON(),
+    address: {
+        city: "London",
+        postCode: "SW9"
+    }
+};
